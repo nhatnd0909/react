9872bb338071950cc9ceca68cd9b1149b7221b5e
Nhat <nguyendinhnhat0909@gmail.com>
2023-06-10 16:39:19 +0700

    React Router Exercise 29

--- src/App.js
+++ src/App.js
@@ -5,36 +5,32 @@ import { Component } from 'react';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         {/* <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//         <Menu dishes={this.state.dishes} /> */}
-//         <Main />
-//       </div>
-//     );
-//   }
-// }
-
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      // <div className="App">
+      //   {/* <Navbar dark color="primary">
+      //     <div className="container">
+      //       <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      //     </div>
+      //   </Navbar>
+      //   <Menu dishes={this.state.dishes} /> */}
+      //   {/* <Main /> */}
+
+
+      // </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
