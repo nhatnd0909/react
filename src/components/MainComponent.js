39e4acf467c70d441889db1b85d93c38c807793b
Nhat <nguyendinhnhat0909@gmail.com>
2023-06-13 14:08:49 +0700

    Single Page Applications Part 1 Exercise 30

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,18 +7,26 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
+
+
 
     onDishSelect(dishId) {
         this.setState({ selectedDish: dishId });
@@ -28,6 +36,9 @@ class Main extends Component {
         const HomePage = () => {
             return (
                 <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
         }
@@ -37,11 +48,10 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Redirect to="/home" />
                 </Switch>
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
                 <Footer />
             </div>
         );
