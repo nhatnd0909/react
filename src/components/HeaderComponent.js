98e21e5c69a939277e01e34501220b1577ef46bd
Nhat <nguyendinhnhat0909@gmail.com>
2023-06-28 13:38:57 +0700

    React Redux Form Validation Exercise 37 update

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -60,11 +60,11 @@ class Header extends Component {
                                 <NavItem>
                                     <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                                 </NavItem>
-                                <NavItem>
-                                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
-                                </NavItem>
                             </Nav>
                         </Collapse>
+                        <NavItem style={{ 'listStyle': 'none' }}>
+                            <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                        </NavItem>
                     </div>
                 </Navbar>
                 <div className="container">
